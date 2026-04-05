45954efb679b8a30e6e0546a6a15d75598dbe2ea
Matvey Morozov <67281212+netrolite@users.noreply.github.com>
2026-04-05 03:24:05 +0300

    init

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+import subprocess
+import platform
+
+
+def ping_ip(ip: str):
+    current_os = platform.system().lower()
+    if current_os == "windows":
+        command = ["ping", "-n", "1", "-w", "0.2", ip]
+    else:
+        # For Linux/macOS
+        command = ["ping", "-c", "1", "-W", "0.2", ip]
+
+    try:
+        # Run the command and suppress standard output and errors
+        result = subprocess.run(
+            command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        # returncode 0 means the ping was successful (pong received)
+        return result.returncode == 0
+    except Exception:
+        return False
+
+
+if __name__ == "__main__":
+    DEFAULT_INPUT_FILE = "scan_result.json"
+    input_file = (
+        input(f"Input filename (Enter for {DEFAULT_INPUT_FILE}): ")
+        or DEFAULT_INPUT_FILE
+    )
+    output_file = "result.json"
+
+    with open(input_file, "r") as f:
+        subnets = json.load(f)
+    result = []
+
+    for subnet, ips in subnets.items():
+        for ip in ips:
+            if ping_ip(ip):
+                print(f"[+] ping {ip} success")
+                result.append(subnet)
+                break
+            else:
+                print(f"ping {ip} timeout")
+
+    with open(output_file, "w") as f:
+        json.dump(result, f)
